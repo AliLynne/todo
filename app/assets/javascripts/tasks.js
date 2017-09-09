8e2d30703bd7eb804f90c07c653434c6345c9875
Ali Thompson <alilynne.thompson@gmail.com>
2017-09-09 20:09:41 +0000

    circle ci added yml file

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,8 +3,8 @@ $(function() {
   // The taskHtml method takes in a Javascript rep. of the task and produces an HTML rep. using <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</lable></div></li>';
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '">' + '<div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</lable></div></li>';
 
     return liElement;
   }
@@ -21,6 +21,11 @@ $(function() {
       task: {
         done: doneValue
       }
+    }).success(function(data){
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
     });
   }
   $.get("/tasks").success( function( data ) {
